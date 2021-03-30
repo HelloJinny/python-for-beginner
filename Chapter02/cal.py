385133935e067c5bb78aca4d1e8e594e8b906d51
HelloJinny <vkfkdto0209@naver.com>
2021-03-30 21:57:40 +0900

    2021.03.25

---
+++ Chapter02/cal.py
@@ -0,0 +1,14 @@
+a = int(input("첫 번째 숫자 입력 : "))
+b = int(input("두 번째 숫자 입력 : "))
+
+result = a + b
+print(a, "+", b, "=", result)
+
+result = a - b
+print(a, "-", b, "=", result)
+
+result = a * b
+print(a, "*", b, "=", result)
+
+result = a / b
+print(a, "/", b, "=", result)
